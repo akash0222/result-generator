cf3557d3b022035f80df07a46378a03a62429987
akash0222 <abhsingh76@gmail.com>
2026-06-23 10:44:20 +0530

    fix api url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000'
+  baseURL: 'https://result-generator-4fru.onrender.com'
 })
 
 API.interceptors.request.use((req) => {
